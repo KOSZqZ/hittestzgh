112e614cae67645901ebd6490bbb09971db94004
web <web@163.com>
2022-08-11 19:25:04 +0800

    dog

---
+++ BaseTestConfig/BaseTestConfig/Dog.h
@@ -0,0 +1,16 @@
+//
+//  Dog.h
+//  BaseTestConfig
+//
+//  Created by ZGH on 2022/8/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BaseTestConfig/BaseTestConfig/Dog.m
@@ -0,0 +1,12 @@
+//
+//  Dog.m
+//  BaseTestConfig
+//
+//  Created by ZGH on 2022/8/11.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
